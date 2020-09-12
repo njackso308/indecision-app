b48de33e5f9cfb747ce2eb99028dedceef0ea9a1
Nick Jackson <nljackson2020@outlook.com>
2020-09-11 22:35:15 -0500

    Saving and loading options data

--- src/app.js
+++ src/app.js
@@ -11,11 +11,25 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('fetching data')
+
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+
+      if (options) {
+        this.setState(() => ({ options }))
+
+      }
+    } catch (e) {
+
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('saving data')
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem('options', json)
+    }
   }
 
   componentWillUnmount() {
@@ -102,6 +116,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started.</p>}
       {
         props.options.map((option) => (
           <Option
@@ -147,7 +162,10 @@ class AddOption extends React.Component {
 
     this.setState(() => ({ error }))
 
-    e.target.elements.option.value = ''
+    if (!error) {
+      e.target.elements.option.value = ''
+    }
+
 
   }
 
