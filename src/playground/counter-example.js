df6b909ff8fa116b727d5cf44c6417a5fd4c8532
Nick Jackson <nljackson2020@outlook.com>
2020-09-11 23:12:02 -0500

    Saving and Loading the count

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,26 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this)
     this.handleReset = this.handleReset.bind(this)
     this.state = {
-      count: props.count
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    try {
+      const stringCount = localStorage.getItem('count')
+      const count = parseInt(stringCount, 10)
+
+      if (!isNaN(count)) {
+        this.setState(() => ({ count }))
+      }
+    } catch (e) {
+
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count)
     }
   }
 
@@ -45,10 +64,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  counts: 0
-}
-
-
-
-ReactDOM.render(<Counter count={5} />, document.getElementById('app'))
+ReactDOM.render(<Counter />, document.getElementById('app'))
