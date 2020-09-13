8955a365ca67f81bd225efd696e23873050e4e4d
Nick Jackson <nljackson2020@outlook.com>
2020-09-13 11:51:53 -0500

    ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('app.js is running')
+// import './utils.js'
+// import { square, add } from './utils.js'
+
+// console.log('app.js is running')
+// console.log(square(4))
+// console.log(add(100, 23))
+
+import { isAdult, canDrink } from './person.js'
+
+console.log(isAdult(17))
+console.log(isAdult(20))
+console.log(canDrink(18))
+console.log(canDrink(30))
