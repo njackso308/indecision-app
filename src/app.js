dacf660b54f50da54d939e149a72ccc777e287c8
Nick Jackson <nljackson2020@outlook.com>
2020-09-13 14:15:04 -0500

    ES6 class properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,28 @@ import ReactDOM from 'react-dom'
 import IndecisionApp from './Components/IndecisionApp'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Mike'
+    this.getGreeting = this.getGreeting.bind(this)
+  }
+  getGreeting() {
+    return `Hi, my name is ${this.name}`
+  }
+}
+
+const oldSyntax = new OldSyntax()
+const getGreeting = oldSyntax.getGreeting
+console.log(getGreeting())
+
+
+class NewSyntax {
+  name = 'Jen'
+  getGreeting = () => {
+    return `Hi, my name is ${this.name}`
+  }
+}
+const newSyntax = new NewSyntax()
+const newGetGretting = newSyntax.getGreeting
+console.log(newGetGretting())
