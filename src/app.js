8d3b0fc743a3bc9a2fe991526d113dd3d864f901
Nick Jackson <nljackson2020@outlook.com>
2020-09-13 12:23:49 -0500

    Importing NPM modules

--- src/app.js
+++ src/app.js
@@ -1,16 +1,5 @@
-// import './utils.js'
-// import subtract, { square, add } from './utils.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// console.log('app.js is running')
-// console.log(square(4))
-// console.log(add(100, 23))
-// console.log(subtract(100, 80))
-
-import isSenior, { isAdult, canDrink } from './person.js'
-
-console.log(isAdult(17))
-console.log(isAdult(20))
-console.log(canDrink(18))
-console.log(canDrink(30))
-console.log(isSenior(55))
-console.log(isSenior(65))
+const template = React.createElement('p', {}, 'Testing 123')
+ReactDOM.render(template, document.getElementById('app'))
