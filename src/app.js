81a3f9f382e19abc29fbcc7fc65eef4dca23af61
Nick Jackson <nljackson2020@outlook.com>
2020-09-13 12:11:27 -0500

    Default Exports

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 // import './utils.js'
-// import { square, add } from './utils.js'
+// import subtract, { square, add } from './utils.js'
 
 // console.log('app.js is running')
 // console.log(square(4))
 // console.log(add(100, 23))
+// console.log(subtract(100, 80))
 
-import { isAdult, canDrink } from './person.js'
+import isSenior, { isAdult, canDrink } from './person.js'
 
 console.log(isAdult(17))
 console.log(isAdult(20))
 console.log(canDrink(18))
 console.log(canDrink(30))
+console.log(isSenior(55))
+console.log(isSenior(65))
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,8 @@ export const square = (x) => x * x
 
 export const add = (a, b) => a + b
 
-// export { square, add }
+export default (a, b) => a - b
+
+// export { square, add, subtract as default }
 
 // exports - default export - named exports
